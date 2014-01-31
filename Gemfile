0046232ea0a8c31c2dcd3df0e4b1af5eea6c55f9
Lukas Spee <lukasspee@gmail.com>
2014-01-31 16:23:42 +0100

    Core functionality

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 gem "sinatra", "~> 1.4.4"
 gem "haml", "~> 4.0.5"
+gem "datamapper", "~> 1.2.0"
+gem "dm-sqlite-adapter", "~> 1.2.0"
--- application.rb
+++ application.rb
@@ -1,10 +1,36 @@
 require "bundler/setup"
 require "sinatra"
+require "dm-core"
+require "dm-migrations"
+require "csv"
+
+DataMapper.setup(:default, "sqlite3:data.db")
+
+class Cheese
+  include DataMapper::Resource
+
+  property :id,     Serial
+  property :name,   String, required: true
+  property :edible, Boolean, required: true
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+CSV.foreach(File.join(__dir__, "data.csv")) do |line|
+  name   = line[0]
+  edible = line[1]
+
+  Cheese.first_or_create({ name: name }, { edible: (edible == "true") })
+end
 
 get "/" do
   haml :index
 end
 
 post "/" do
-  haml :results, locals: { query: params[:query] }
+  query  = "%#{params[:query]}%"
+  results = Cheese.all(:name.like => query)
+
+  haml :results, locals: { results: results }
 end
